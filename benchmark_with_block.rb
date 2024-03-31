6b86b0f44f512dbeeaf79d1846ddb284a34a7cf7
wingkoon <wingkoon@yahoo.com>
2024-03-31 12:29:43 -0700

    benchmark

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,10 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  running_time = end_time - start_time
+  running_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
